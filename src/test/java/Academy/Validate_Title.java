269d67bd7a7e440b7683962a7c5a1f77e83d83c4
Soumya <soumyaranjanlenka233@gmail.com>
2020-07-24 21:41:16 +0530

    changed done

--- src/test/java/Academy/Validate_Title.java
+++ src/test/java/Academy/Validate_Title.java
@@ -13,41 +13,35 @@ import PageObjects.Landing_Page;
 
 public class Validate_Title extends Base {
 	WebDriver driver;
-	Logger log=LogManager.getLogger(Home_Page.class.getName());
+	Logger log = LogManager.getLogger(Home_Page.class.getName());
 
 	@BeforeTest
-	public void initialize() throws IOException
-	{
-		driver=initializeDriver();
+	public void initialize() throws IOException {
+		driver = initializeDriver();
 		driver.get(prop.getProperty("url"));
 	}
 
 	@Test
-	public void basePageNavigation()
-	{
-		
-		Landing_Page lp= new Landing_Page(driver);
-		
-		Assert.assertEquals(lp.getTitle().getText(), "Featured Courses");
-		
-	}
-	
-	@Test
-	public void gitExample()
-	{
-				System.out.println("Just for Git Example1 Purpose");	
-				System.out.println("Just for Git Example2 Purpose");
-				System.out.println("newly add file");
+	public void basePageNavigation() {
 
-				
+		Landing_Page lp = new Landing_Page(driver);
+
+		Assert.assertEquals(lp.getTitle().getText(), "Featured Courses");
+
 	}
-	
-	
+
+	@Test
+	public void gitExample() {
+		System.out.println("Just for Git Example1 Purpose");
+		System.out.println("Just for Git Example2 Purpose");
+		System.out.println("newly add file");
+
+	}
+
 	@AfterTest
-	public void shutdown()
-	{
+	public void shutdown() {
 		driver.close();
-		
+
 	}
 
 }
